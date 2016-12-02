123e3b79f609fe7b36421694e241a34ca60262ba
crasnier <valentin@shopmycourses.com>
2016-12-02 16:37:52 +0100

    annonce new + create

--- app/controllers/annonces_controller.rb
+++ app/controllers/annonces_controller.rb
@@ -4,4 +4,23 @@ class AnnoncesController < ApplicationController
     @annonces = Annonce.all
   end
 
+  def new
+    @annonce = Annonce.new
+  end
+
+  def create
+    annonce = Annonce.new(annonce_params)
+    annonce.user_id = current_user.id
+    annonce.save
+    redirect_to root_path
+  end
+
+
+
+  private
+
+      # Never trust parameters from the scary internet, only allow the white list through.
+    def annonce_params
+      params.require(:annonce).permit(:user_id, :titre, :description, :prix, :image)
+    end
 end
--- app/models/annonce.rb
+++ app/models/annonce.rb
@@ -1,2 +1,9 @@
 class Annonce < ApplicationRecord
+
+belongs_to :user
+
+  def self.getAnnoncesByUser id
+    tableau = Annonce.where(user_id: id)
+    return tableau
+  end
 end
